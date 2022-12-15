7ffd154829ca850dd6aa4c19c52f53a4207bca6e
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2022-12-15 17:47:21 +0800

    edited html to retrieve screenshot with all colours, added content to README and commented the js script

--- Assets/script.js
+++ Assets/script.js
@@ -13,6 +13,7 @@ var entryArr = JSON.parse(localStorage.getItem("entryArr")) || {
   "hour-15": "",
   "hour-16": "",
   "hour-17": ""
+  
 };
 
 
@@ -29,20 +30,20 @@ $("#currentTime").append(displayTime);
 
 
 
-// Click event to populate the tasksObj with text input from the textarea 
+//  ---- 'Click' event to store the entries from the text area ---- // 
 $("button").on("click", function () {
 
-  // get id
+  
   console.log($(this).parent().attr("id"));
   var entryId = $(this).parent().attr("id");
 
-  // get textarea input
+
   var textInput = $(this).parent().find("textarea");
   textInput.val();
   console.log(textInput.val());
   
 
-  // printing the object array in the console to make sure it's there
+  // ---- printing array in the console to make sure the array has been stored ---- //
   console.log("Entries: ", entryArr);
 
   entryArr[entryId] = textInput.val();
@@ -51,7 +52,7 @@ $("button").on("click", function () {
 });
 
 
-// ---- Stores entries to local storage ---- //
+// ---- Function to store entries to local storage ---- //
 function storeEntries() {
 
   localStorage.setItem("entryArr", JSON.stringify(entryArr));
@@ -61,7 +62,7 @@ function storeEntries() {
 
 
 
-
+// ---- function to re-populate the time-blocks text areas ---- //
 function onLoad(){
   for (var i = 0; i < 9; i++) {
     
---
+++ README.md
@@ -0,0 +1,48 @@
+# Pop Quiz Time
+
+## Description
+The following application provides the user with a daily work journal that can be used to store their activities through the day. In this case, the application has been customed to fit the standard business hours (9am-5pm), but can be changed if the user requires it.
+
+The current version of this work journal, displays the different times of the work day in 3 different colours (representing past-present-future activities). The script interacts with the live time API to assign and remove classes accordingly.
+
+-------------------
+
+## Instructions
+1. Click on the provided URL for the deployed site.
+2. Enter any tasks you have on the present day.
+3. Click the save button to make sure the task is stored in the browser.
+4. Your daily activities will be displayed even if you close the window.
+5. Edit the entries if you need to change your tasks through the day.
+
+
+
+## Installation
+
+N/A
+
+--------------------
+
+## Submission Links
+
+### Deployed URL: https://github.com/MoMoncada/work-journal
+
+### GitHub: 
+
+---------------------
+
+## Deployed Web Page
+
+### Work Journal
+![Full of tasks](./Assets/past-present-future%20example.png)
+
+
+
+
+
+## Credits
+Maria Auxiliadora Moncada 
+
+------------
+
+## License
+N/A
